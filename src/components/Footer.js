b2882f9a3eae292f0249b6ddcfaaf4bbde8590d0
dulce valeria guzman taveras <Blastomm@hotmail.com>
2022-01-13 11:36:41 -0400

    done footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
     return(
         <div className="class-Footer">
-Footer
+            Make with 💕 by Dulce Valeriana 
         </div>
     )
 }
--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -4,10 +4,10 @@ import { SocialIcon } from 'react-social-icons';
 export default function SocialMedia() {
     return(
         <div className="class-SocialMedia">
-            <SocialIcon url="https://twitter.com/" />
-            <SocialIcon url="https://facebook.com/" />
-            <SocialIcon url="https://youtube.com/" />
-            <SocialIcon url="https://pinterest.com/" />
+            <SocialIcon url="https://twitter.com/" target="_black"/>
+            <SocialIcon url="https://facebook.com/" target="_black"/>
+            <SocialIcon url="https://youtube.com/" target="_black"/>
+            <SocialIcon url="https://pinterest.com/" target="_black"/>
         </div>
     )
 }
