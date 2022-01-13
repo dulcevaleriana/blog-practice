bf196da106668d240c8532605bfb6a85255b67d5
dulce valeria guzman taveras <Blastomm@hotmail.com>
2022-01-13 11:28:28 -0400

    social media done

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { SocialIcon } from 'react-social-icons';
 
 export default function SocialMedia() {
     return(
         <div className="class-SocialMedia">
-            SocialMedia
+            <SocialIcon url="https://twitter.com/" />
+            <SocialIcon url="https://facebook.com/" />
+            <SocialIcon url="https://youtube.com/" />
+            <SocialIcon url="https://pinterest.com/" />
         </div>
     )
 }
