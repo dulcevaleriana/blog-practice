678793edd3cc4193c69ea765c3e0f24c1447b634
dulce valeria guzman taveras <Blastomm@hotmail.com>
2022-01-13 12:24:49 -0400

    first step comment component

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,9 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faComment } from '@fortawesome/free-solid-svg-icons';
+import { faClose } from '@fortawesome/free-solid-svg-icons';
 
 export default function Comments() {
+    const [commentBoolean, setCommentBoolean] = useState(false);
+
     return(
         <div className="class-Comments">
-Comments
+            {commentBoolean ? (
+                <button onClick={() => setCommentBoolean(false)}>
+                    <FontAwesomeIcon icon={faClose} />
+                </button>
+            )
+            :
+            (
+                <button onClick={() => setCommentBoolean(true)}>
+                    <FontAwesomeIcon icon={faComment} />
+                </button>
+            )}
+            {commentBoolean && (
+                <div>
+                    <form>
+                        <h5>Commentss</h5>
+                        <input name="name" type="text" placeholder="Write your name"/>
+                        <textarea />
+                        <button>
+                            send
+                        </button>
+                    </form>
+                    <div>
+                        <div>
+                            <h6>Name</h6>
+                            <p>
+                                comment
+                            </p>
+                            <hr/>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
