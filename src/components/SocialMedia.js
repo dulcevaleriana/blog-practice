036226b78cba1463b7ec35549161e29d32012c13
dulce valeria guzman taveras <Blastomm@hotmail.com>
2022-01-13 12:49:44 -0400

    done ajust social media style

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import { SocialIcon } from 'react-social-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import { faAngleLeft } from '@fortawesome/free-solid-svg-icons';
 
 export default function SocialMedia() {
+    const [socialMediaBoolean, setSocialMediaBoolean] = useState(false);
+
     return(
         <div className="class-SocialMedia">
-            <SocialIcon url="https://twitter.com/" target="_black"/>
-            <SocialIcon url="https://facebook.com/" target="_black"/>
-            <SocialIcon url="https://youtube.com/" target="_black"/>
-            <SocialIcon url="https://pinterest.com/" target="_black"/>
+        {socialMediaBoolean ? <FontAwesomeIcon icon={faAngleLeft} onClick={() => setSocialMediaBoolean(false)} />:<FontAwesomeIcon icon={faAngleRight} onClick={() => setSocialMediaBoolean(true)} />}
+        {socialMediaBoolean && (
+            <div>
+                <SocialIcon url="https://twitter.com/" target="_black"/>
+                <SocialIcon url="https://facebook.com/" target="_black"/>
+                <SocialIcon url="https://youtube.com/" target="_black"/>
+                <SocialIcon url="https://pinterest.com/" target="_black"/>
+            </div>
+        )}
         </div>
     )
 }
